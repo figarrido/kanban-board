6ddfbf7a0c6b0af480c267338fea9c6a78becbc3
Felipe Garrido <figarrido@uc.cl>
2018-12-09 02:42:08 -0300

    fix(tests): update tests after refactoring

--- __test__/resolvers/board.test.js
+++ __test__/resolvers/board.test.js
@@ -3,7 +3,7 @@ const {
   filter,
   pipe,
   propEq,
-  test,
+  find,
   merge,
   pick,
 } = require('ramda')
@@ -18,9 +18,9 @@ const {
 
 describe('Board resolver', () => {
   const db = [
-    { id: '1', name: 'First board', save: jest.fn(), remove: jest.fn() },
-    { id: '2', name: 'Second board', save: jest.fn(), remove: jest.fn() },
-    { id: '3', name: 'Third board', save: jest.fn(), remove: jest.fn() },
+    { id: '1', name: 'First board' },
+    { id: '2', name: 'Second board' },
+    { id: '3', name: 'Third board' },
   ]
 
   beforeEach(jest.clearAllMocks)
@@ -48,8 +48,8 @@ describe('Board resolver', () => {
   })
 
   it('should return a list of boards', async () => {
-    const find = jest.fn(({ name }) => filter(
-      obj => test(new RegExp(name, 'i'), obj.name)
+    const where = jest.fn(({ name }) => filter(
+      obj => obj.name.toLowerCase().includes(name.toLowerCase())
     )(db))
 
     const parent = {}
@@ -57,15 +57,15 @@ describe('Board resolver', () => {
     const ctx = {
       models: {
         Board: {
-          find,
+          where,
         },
       },
     }
 
     const boards = await getBoardsByName(parent, args, ctx)
     expect(boards).toEqual(db)
-    expect(find.mock.calls).toHaveLength(1)
-    expect(find.mock.calls[0][0]).toEqual({ name: new RegExp(args.name, 'i') })
+    expect(where.mock.calls).toHaveLength(1)
+    expect(where.mock.calls[0][0]).toEqual({ name: args.name })
 
   })
 
@@ -89,38 +89,40 @@ describe('Board resolver', () => {
   })
 
   it('should update a board by id', async () => {
+    const updateWith = jest.fn((id, fields) => merge(find(propEq('id', id), db), fields))
+
     const parent = {}
     const args = { id: '1', name: 'Board' }
     const ctx = {
       models: {
         Board: {
-          findById,
+          updateWith,
         },
       },
     }
 
     const updatedBoard = await updateBoard(parent, args, ctx)
     expect(pick(['id', 'name'], updatedBoard)).toEqual(merge(args, { id: '1' }))
-    expect(db[0].save.mock.calls).toHaveLength(1)
-    expect(findById.mock.calls).toHaveLength(1)
-    expect(findById.mock.calls[0][0]).toEqual(args.id)
+    expect(updateWith.mock.calls).toHaveLength(1)
+    expect(updateWith.mock.calls[0][0]).toEqual(args.id)
   })
 
   it('should update a board by id', async () => {
+    const removeWithId = jest.fn(id => find(propEq('id', id), db))
+
     const parent = {}
     const args = { id: '1' }
     const ctx = {
       models: {
         Board: {
-          findById,
+          removeWithId,
         },
       },
     }
 
     const deletedBoard = await deleteBoard(parent, args, ctx)
     expect(pick(['id', 'name'], deletedBoard)).toEqual(pick(['id', 'name'], db[0]))
-    expect(db[0].remove.mock.calls).toHaveLength(1)
-    expect(findById.mock.calls).toHaveLength(1)
-    expect(findById.mock.calls[0][0]).toEqual(args.id)
+    expect(removeWithId.mock.calls).toHaveLength(1)
+    expect(removeWithId.mock.calls[0][0]).toEqual(args.id)
   })
 })
